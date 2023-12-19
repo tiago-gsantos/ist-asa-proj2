98f4f433f085697db0a8fd4630d931e53fcc7a19
Tiago Santos <tiago.gouveia.santos@tecnico.ulisboa.pt>
2023-12-19 10:18:39 +0000

    Done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <list>
 #include <vector>
 #include <stack>
 #include <cmath>
@@ -14,7 +13,7 @@ struct vertice {
   int sccID;
 };
 
-std::stack<vertice*> DFS(std::list<int> *graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack){
+std::stack<vertice*> DFS(std::vector<std::vector<unsigned int>> &graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack){
   std::stack<vertice*> orderedByFinishTime;
   
   while(!stack.empty()){
@@ -44,7 +43,7 @@ std::stack<vertice*> DFS(std::list<int> *graph, std::vector<vertice> &vertices, 
 }
 
 
-int calculateSCCs(std::list<int> *graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack){
+int calculateSCCs(std::vector<std::vector<unsigned int>> &graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack){
   int scc = 1;
   int verticesInSCC = 0;
 
@@ -78,7 +77,7 @@ int calculateSCCs(std::list<int> *graph, std::vector<vertice> &vertices, std::st
 }
 
 
-int calculateMaxJumps(std::list<int> *graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack, int numSCC){
+int calculateMaxJumps(std::vector<std::vector<unsigned int>> &graph, std::vector<vertice> &vertices, std::stack<vertice*> &stack, int numSCC){
   std::vector<int> sccJumps(numSCC + 1, 0);
   int maxJumps = 0;
   
@@ -90,7 +89,7 @@ int calculateMaxJumps(std::list<int> *graph, std::vector<vertice> &vertices, std
       if(vertices[adjVertice].sccID == v->sccID)
         sccJumps[v->sccID] = std::max(sccJumps[v->sccID], sccJumps[vertices[adjVertice].sccID]);
       else
-        sccJumps[v->sccID] = std::max(sccJumps[v->sccID], sccJumps[vertices[adjVertice].sccID] +1);
+        sccJumps[v->sccID] = std::max(sccJumps[v->sccID], sccJumps[vertices[adjVertice].sccID] + 1);
       
       maxJumps = std::max(maxJumps, sccJumps[v->sccID]);
     }
@@ -100,6 +99,8 @@ int calculateMaxJumps(std::list<int> *graph, std::vector<vertice> &vertices, std
 
 
 int main(){
+  std::ios::sync_with_stdio(0); // disable sync with c libs
+  std::cin.tie(0); // discard cin buffer after each line of input
   unsigned int V, E;
 
   // Read Vertices and Edges
@@ -107,8 +108,8 @@ int main(){
   std::cin >> E;
 
   // Create Graph and Trnspost Graph
-  std::list<int> *graph = new std::list<int>[V + 1];
-  std::list<int> *transpostGraph = new std::list<int>[V + 1];
+  std::vector<std::vector<unsigned int>> graph(V + 1);
+  std::vector<std::vector<unsigned int>> transpostGraph(V + 1);
 
   // Initialize vector with all vertices
   std::vector<vertice> vertices(V+1);
